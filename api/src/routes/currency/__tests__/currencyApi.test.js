0322c8083ea717a18694d8d9be21587449d0c798
niroshn <niroshanrd.13@cse.mrt.ac.lk>
2019-06-27 00:49:18 +0530

    unit test for backend app

--- api/src/routes/currency/__tests__/currencyApi.test.js
+++ api/src/routes/currency/__tests__/currencyApi.test.js
@@ -2,10 +2,12 @@ import HttpStatus from 'http-status-codes';
 import request from 'supertest';
 import { app } from '../../../index';
 
-describe('routes: get /hello', () => {
+jest.mock('../__mocks__/currencyService');
+
+describe('routes: get /currency/info', () => {
   test('should respond with success status', async () => {
     const response = await request(app)
-      .get('/hello')
+      .get('/currency/info')
       .set('Accept', 'application/json');
     expect(response.status).toEqual(HttpStatus.OK);
     expect(response.body).toBeDefined();
